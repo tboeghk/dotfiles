34533ffd4d2b8b7852f562768f8510d2a67f9a85
Torsten Bøgh Köster <torsten.koester@s24.com>
2019-05-30 22:54:35 +0200

    reduce brew tool clutter

--- brew.sh
+++ brew.sh
@@ -43,8 +43,8 @@ brew install vim --with-override-system-vi
 brew install grep
 brew install openssh
 brew install screen
-brew install php
-brew install gmp
+#brew install php
+#brew install gmp
 
 # Install font tools.
 brew tap bramstein/webfonttools
@@ -53,30 +53,30 @@ brew install sfnt2woff-zopfli
 brew install woff2
 
 # Install some CTF tools; see https://github.com/ctfs/write-ups.
-brew install aircrack-ng
-brew install bfg
-brew install binutils
-brew install binwalk
-brew install cifer
-brew install dex2jar
-brew install dns2tcp
-brew install fcrackzip
-brew install foremost
-brew install hashpump
-brew install hydra
-brew install john
-brew install knock
-brew install netpbm
-brew install nmap
-brew install pngcheck
+#brew install aircrack-ng
+#brew install bfg
+#brew install binutils
+#brew install binwalk
+#brew install cifer
+#brew install dex2jar
+#brew install dns2tcp
+#brew install fcrackzip
+#brew install foremost
+#brew install hashpump
+#brew install hydra
+#brew install john
+#brew install knock
+#brew install netpbm
+#brew install nmap
+#brew install pngcheck
 brew install socat
-brew install sqlmap
-brew install tcpflow
-brew install tcpreplay
-brew install tcptrace
-brew install ucspi-tcp # `tcpserver` etc.
-brew install xpdf
-brew install xz
+#brew install sqlmap
+#brew install tcpflow
+#brew install tcpreplay
+#brew install tcptrace
+#brew install ucspi-tcp # `tcpserver` etc.
+#brew install xpdf
+#brew install xz
 
 # Install other useful binaries.
 brew install ack
@@ -84,17 +84,17 @@ brew install ack
 brew install git
 brew install git-lfs
 brew install imagemagick --with-webp
-brew install lua
-brew install lynx
-brew install p7zip
-brew install pigz
-brew install pv
-brew install rename
-brew install rlwrap
+#brew install lua
+#brew install lynx
+#brew install p7zip
+#brew install pigz
+#brew install pv
+#brew install rename
+#brew install rlwrap
 brew install ssh-copy-id
 brew install tree
-brew install vbindiff
-brew install zopfli
+#brew install vbindiff
+#brew install zopfli
 
 # Install stuff from Brewfile
 brew bundle init/Brewfile
