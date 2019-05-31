aad8e3dac5c05b2bc0f96dcfb113a588f6019a85
Torsten Köster <torsten@TorstensMacBookPro.fritz.box>
2019-05-31 10:52:16 +0200

    Update Brewfile

--- brew.sh
+++ brew.sh
@@ -1,5 +1,8 @@
 #!/usr/bin/env bash
 
+# copy global Brewfile in place
+cp .Brewfile ~/.Brewfile
+
 # Install command-line tools using Homebrew.
 
 # Make sure we’re using the latest Homebrew.
@@ -21,7 +24,7 @@ brew install moreutils
 # Install GNU `find`, `locate`, `updatedb`, and `xargs`, `g`-prefixed.
 brew install findutils
 # Install GNU `sed`, overwriting the built-in `sed`.
-brew install gnu-sed --with-default-names
+brew install gnu-sed
 # Install Bash 4.
 brew install bash
 brew install bash-completion2
@@ -32,25 +35,11 @@ if ! fgrep -q "${BREW_PREFIX}/bin/bash" /etc/shells; then
   chsh -s "${BREW_PREFIX}/bin/bash";
 fi;
 
-# Install `wget` with IRI support.
-brew install wget --with-iri
+# Install stuff from Brewfile
+brew bundle --global
 
-# Install GnuPG to enable PGP-signing commits.
-brew install gnupg
-
-# Install more recent versions of some macOS tools.
-brew install vim --with-override-system-vi
-brew install grep
-brew install openssh
-brew install screen
-#brew install php
-#brew install gmp
-
-# Install font tools.
-brew tap bramstein/webfonttools
-brew install sfnt2woff
-brew install sfnt2woff-zopfli
-brew install woff2
+# Remove outdated versions from the cellar.
+brew cleanup
 
 # Install some CTF tools; see https://github.com/ctfs/write-ups.
 #brew install aircrack-ng
@@ -69,7 +58,6 @@ brew install woff2
 #brew install netpbm
 #brew install nmap
 #brew install pngcheck
-brew install socat
 #brew install sqlmap
 #brew install tcpflow
 #brew install tcpreplay
@@ -79,11 +67,7 @@ brew install socat
 #brew install xz
 
 # Install other useful binaries.
-brew install ack
 #brew install exiv2
-brew install git
-brew install git-lfs
-brew install imagemagick
 #brew install lua
 #brew install lynx
 #brew install p7zip
@@ -91,13 +75,5 @@ brew install imagemagick
 #brew install pv
 #brew install rename
 #brew install rlwrap
-brew install ssh-copy-id
-brew install tree
 #brew install vbindiff
 #brew install zopfli
-
-# Install stuff from Brewfile
-brew bundle --global
-
-# Remove outdated versions from the cellar.
-brew cleanup
